3d7d85052c986f69099dab953f47ccff8a264058
keyouts <keyouts@outlook.com>
2025-07-08 21:03:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-f
+ const el = document.getElementById('typed');
+  const rawText = document.getElementById('typewriter').getAttribute('data-text');
+  let i = 0;
+  let isTag = false;
+  let htmlBuffer = '';
+
+  function type() {
+    if (i < rawText.length) {
+      const char = rawText[i];
+      htmlBuffer += char;
+
+      if (char === '<') isTag = true;
+      if (char === '>') isTag = false;
+
+      if (!isTag) {
+        el.innerHTML = htmlBuffer;
+      }
+
+      i++;
+      setTimeout(type, 50); // speed
+    }
+  }
+
+  window.onload = type;
+
